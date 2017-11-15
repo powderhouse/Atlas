b2fb4edd38ffabf13147f7756edb113dd28be245
Alec Resnick <aresnickety@gmail.com>
2017-11-15 01:24:55 -0500

    Initial Commit

---
+++ atlas/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  atlas
+//
+//  Created by Alec Resnick on 11/15/17.
+//  Copyright © 2017 Powderhouse Studios. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ atlas/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  atlas
+//
+//  Created by Alec Resnick on 11/15/17.
+//  Copyright © 2017 Powderhouse Studios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
