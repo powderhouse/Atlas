a32281b8064ae97cad3029aed7701e6bbecafee5
Jared Cosulich <jared.cosulich@gmail.com>
2017-12-12 21:06:47 -0500

    refactoring project into its own file

--- atlas/lib/Projects.swift
+++ atlas/lib/Projects.swift
@@ -8,53 +8,6 @@
 
 import Foundation
 
-class Project {
-    let directory: URL!
-    let staging: URL!
-    var name: String!
-    var files: [String]!
-    var stagedFiles: [String]!
-    
-    let ignore = [
-        "readme.md",
-        "staging"
-    ]
-    
-    init(_ directory: URL) {
-        self.directory = directory
-        self.name = directory.lastPathComponent
-        self.staging = directory.appendingPathComponent("staging")
-
-        self.files = getFiles()
-        self.stagedFiles = getFiles(staging)
-    }
-    
-    func stageFile(_ path: String) {
-        guard directory != nil else { return }
-        _ = Glue.runProcess("cp", arguments: [path, staging.path])
-        self.stagedFiles = getFiles(staging)
-        
-        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "project-staged-files"), object: self)
-    }
-
-    func getFiles(_ url: URL?=nil) -> [String] {
-        let inDirectory = (url ?? directory)!
-        let fileManager = FileManager.default
-        
-        let contents = try? fileManager.contentsOfDirectory(
-            at: inDirectory,
-            includingPropertiesForKeys: [URLResourceKey.isDirectoryKey]
-        )
-        
-        guard contents != nil else {
-            return []
-        }
-        
-        let filteredContents = contents!.filter { !ignore.contains($0.lastPathComponent) }
-        return filteredContents.map { $0.lastPathComponent }.sorted()
-    }
-}
-
 class Projects {
     
     let atlasRepository: URL!
