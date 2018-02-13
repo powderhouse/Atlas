e120a57fe9d9eccf20a88ad386735b70290297d6
Jared Cosulich <jared.cosulich@gmail.com>
2018-02-12 20:57:55 -0500

    experimenting with Commander

--- Sources/Atlas/main.swift
+++ Sources/Atlas/main.swift
@@ -1,3 +1,33 @@
+import Commander
 import AtlasCore
 
-AtlasCore.hello()
+let main = command { (filename:String) in
+    print("Reading file \(filename)...")
+}
+
+let group = Group {
+
+    print("$0: \($0)")
+    
+//    $0.command(
+//        "login",
+//        description: "TEST",
+//        Option("username")
+//    ) { (username:String) in
+//        print("Hello \(username)")
+//    }
+
+    $0.command("logout") {
+        print("Goodbye.")
+    }
+    
+    $0.command("test") { (parser:ArgumentParser) in
+        print("HAS V: \(parser.hasOption("v")) - \(parser)")
+    }
+}
+    
+group.command("test", "TEST") { (test:String) in
+    print("TEST: \(test)")
+}
+    
+group.run()
