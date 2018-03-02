25a6daec6f237e5d5c92dd37952410c71cc1b27b
Jared Cosulich <jared.cosulich@gmail.com>
2018-03-02 14:20:49 -0500

    testing simplified travis.yml

--- AtlasAppUITests/AtlasAppUITests.swift
+++ AtlasAppUITests/AtlasAppUITests.swift
@@ -30,7 +30,7 @@ class AtlasAppUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssert(false)
+        XCTAssert(true)
     }
     
 }
