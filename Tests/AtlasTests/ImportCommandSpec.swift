69b6ef0f0d3c2b6fadab144cd63bddc85be1b8aa
Jared Cosulich <jared.cosulich@gmail.com>
2018-03-03 22:07:20 -0500

    circleci config

--- Tests/AtlasTests/ImportCommandSpec.swift
+++ Tests/AtlasTests/ImportCommandSpec.swift
@@ -84,7 +84,7 @@ class ImportCommandSpec: QuickSpec {
                 it("should leave both files in the original folder") {
                     for file in [file1, file2] {
                         let exists = fileManager.fileExists(atPath: file!.path, isDirectory: &isFile)
-                        expect(exists).to(beFalse(), description: "Original file not found")
+                        expect(exists).to(beTrue(), description: "Original file not found")
                     }
                 }
 
