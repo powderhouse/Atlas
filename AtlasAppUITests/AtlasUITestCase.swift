b2ef75f5ddb2471b7e192e9b83b4ae4eb6eb2f04
Jared Cosulich <jared.cosulich@gmail.com>
2018-08-03 16:40:43 -0400

    not using github any more, no need to delete

--- AtlasAppUITests/AtlasUITestCase.swift
+++ AtlasAppUITests/AtlasUITestCase.swift
@@ -48,25 +48,6 @@ class AtlasUITestCase: XCTestCase {
     }
     
     func reset() {
-        let credentials = testDirectory.appendingPathComponent("credentials.json")
-        let json = try? String(contentsOf: credentials, encoding: .utf8)
-        if let data = json?.data(using: .utf8) {
-            do {
-                if let credentialsDict = try JSONSerialization.jsonObject(with: data, options: []) as? [String: String] {
-                    if let username = credentialsDict["username"] {
-                        if let token = credentialsDict["token"] {
-                            _ = Glue.runProcess("curl", arguments: [
-                                "-u", "\(username):\(token)",
-                                "-X", "DELETE",
-                                "https://api.github.com/repos/\(username)/\(AtlasCore.repositoryName)"
-                                ])
-                        }
-                    }
-                }
-            } catch {
-            }
-        }
-        
         while FileSystem.fileExists(testDirectory, isDirectory: true) {
             _ = Glue.runProcess(
                 "chmod",
