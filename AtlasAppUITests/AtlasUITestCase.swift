c948f8c91b25bb521fc3141389f49e998693e8fe
Jared Cosulich <jared.cosulich@gmail.com>
2018-10-08 17:02:46 -0400

    trying to get tests working again

--- AtlasAppUITests/AtlasUITestCase.swift
+++ AtlasAppUITests/AtlasUITestCase.swift
@@ -44,6 +44,7 @@ class AtlasUITestCase: XCTestCase {
         
 //        reset()
         app.launch()
+        app.activate()
         
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
         
