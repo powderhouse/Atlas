4a58246f1b24476c917fc4c24b07b7a9bc7d0d23
Jared Cosulich <jared.cosulich@gmail.com>
2018-11-14 13:09:47 -0500

    new version of git-annex

--- Sources/Atlas/main.swift
+++ Sources/Atlas/main.swift
@@ -2,7 +2,7 @@ import SwiftCLI
 import AtlasCore
 import AtlasCommands
 
-let cli = CLI(name: "Atlas", version: "0.4.1", description: "Powderhouse Studios Atlas")
+let cli = CLI(name: "Atlas", version: "0.4.2", description: "Powderhouse Studios Atlas")
 
 let atlasCore: AtlasCore = AtlasCore()
 _ = atlasCore.initialize()
