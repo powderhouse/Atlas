ca92e3900ba21aa54cb10c878d53f3535946e9b3
Jared Cosulich <jared.cosulich@gmail.com>
2018-11-16 17:23:43 -0500

    updating atlascore

--- Sources/Atlas/main.swift
+++ Sources/Atlas/main.swift
@@ -2,7 +2,7 @@ import SwiftCLI
 import AtlasCore
 import AtlasCommands
 
-let cli = CLI(name: "Atlas", version: "0.4.4", description: "Powderhouse Studios Atlas")
+let cli = CLI(name: "Atlas", version: "0.4.5", description: "Powderhouse Studios Atlas")
 
 let atlasCore: AtlasCore = AtlasCore()
 _ = atlasCore.initialize()
