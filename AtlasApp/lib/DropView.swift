ea11243fee8525a3a2ee0975189c61a9b9626fd6
Jared Cosulich <jared.cosulich@gmail.com>
2018-12-07 16:54:27 -0500

    drag and drop in files from the photo app

--- AtlasApp/lib/DropView.swift
+++ AtlasApp/lib/DropView.swift
@@ -14,11 +14,23 @@ class DropView: NSView {
     var filePath: String?
     var project: Project?
     
+    var filePromiseProcessingCount = 0
+    
+    /// queue used for reading and writing file promises
+    private lazy var workQueue: OperationQueue = {
+        let providerQueue = OperationQueue()
+        providerQueue.qualityOfService = .userInitiated
+        return providerQueue
+    }()
+    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
 
         self.wantsLayer = true
-        registerForDraggedTypes([NSPasteboard.PasteboardType.URL, NSPasteboard.PasteboardType.fileURL])
+        
+        var dragTypes = [NSPasteboard.PasteboardType.URL, NSPasteboard.PasteboardType.fileURL]
+        dragTypes += NSFilePromiseReceiver.readableDraggedTypes.map { NSPasteboard.PasteboardType($0) }
+        registerForDraggedTypes(dragTypes)
     }
     
     override func draw(_ dirtyRect: NSRect) {
@@ -26,24 +38,73 @@ class DropView: NSView {
     }
     
     override func draggingEntered(_ sender: NSDraggingInfo) -> NSDragOperation {
-        self.layer?.backgroundColor = NSColor.green.cgColor
+        highlight()
         return .copy
     }
     
     override func draggingExited(_ sender: NSDraggingInfo?) {
-        self.layer?.backgroundColor = NSColor.gray.withAlphaComponent(0).cgColor
+        stopHighlight()
     }
     
     override func draggingEnded(_ sender: NSDraggingInfo) {
-        self.layer?.backgroundColor = NSColor.gray.withAlphaComponent(0).cgColor
+        stopHighlight()
     }
     
     override func performDragOperation(_ sender: NSDraggingInfo) -> Bool {
-        guard let paths = sender.draggingPasteboard().propertyList(forType: NSPasteboard.PasteboardType(rawValue: "NSFilenamesPboardType")) as? [String]
-            else { return false }
-        
         guard project != nil else { return false }
+
+        highlight()
+        sender.enumerateDraggingItems(options: [], for: self, classes: [NSFilePromiseReceiver.self], searchOptions: [:], using: {(draggingItem, idx, stop) in
+            
+            if let filePromiseReceiver = draggingItem.item as? NSFilePromiseReceiver {
+                self.filePromiseProcessingCount += 1
+                if let stagedDirectory = self.project?.directory(Project.staged) {
+                    filePromiseReceiver.receivePromisedFiles(
+                        atDestination: stagedDirectory,
+                        options: [:],
+                        operationQueue: self.workQueue
+                    ) { (fileURL, error) in
+                        Terminal.log("Processing file...")
+                        if let error = error {
+                            Terminal.log("Unable to process file: \(error)")
+                        } else {
+                            self.processPaths([fileURL.path])
+                        }
+                        
+                        self.filePromiseProcessingCount -= 1
+                        if self.filePromiseProcessingCount == 0 {
+                            DispatchQueue.main.async(execute: {
+                                self.stopHighlight()
+                            })
+                        }
+                    }
+                }
+            }
+        })
         
+        if filePromiseProcessingCount == 0 {
+            if let paths = sender.draggingPasteboard().propertyList(forType: NSPasteboard.PasteboardType(rawValue: "NSFilenamesPboardType")) as? [String] {
+                processPaths(paths)
+            }
+        
+            stopHighlight()
+        }
+        
+        return true
+    }
+    
+    func highlight() {
+        layer?.backgroundColor = NSColor.green.cgColor
+    }
+    
+    func stopHighlight() {
+        guard filePromiseProcessingCount == 0 else {
+            return
+        }
+        self.layer?.backgroundColor = NSColor.gray.withAlphaComponent(0).cgColor
+    }
+    
+    func processPaths(_ paths: [String]) {
         DispatchQueue.global(qos: .background).async {
             let result = self.project!.copyInto(paths)
             if result.success {
@@ -57,6 +118,5 @@ class DropView: NSView {
                 })
             }
         }
-        return true
     }
 }
