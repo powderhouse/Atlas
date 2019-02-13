db2b1b5eb30d98f00a4e0610267abd2e3720d2e3
Jared Cosulich <jared.cosulich@gmail.com>
2019-02-13 10:07:13 -0500

    use a timeout instead of a listener to trigger page save

--- git/postprocessing/chromeextension/src/index.js
+++ git/postprocessing/chromeextension/src/index.js
@@ -1,40 +1,43 @@
 import $ from "jquery";
 import mhtml2html from 'mhtml2html';
 
+function savePage(tab) {
+  chrome.pageCapture.saveAsMHTML({tabId: tab.id}, function(mhtmlBlob) {
+    var fr = new FileReader();
+    fr.onload = function () {
+        var mhtmlData = fr.result;
+        const html = mhtml2html.convert(mhtmlData);
+
+        const formData = new FormData();
+        formData.append("html", html.documentElement.outerHTML);
+        formData.append("title", tab.title)
+        $.ajax({
+            url: 'http://localhost:1111/save',
+            method: 'POST',
+            type: 'POST',
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            complete: function() {
+              alert("Page saved...")
+            }
+        });
+    };
+    fr.readAsBinaryString(mhtmlBlob);
+  });
+}
+
 chrome.browserAction.onClicked.addListener(async function(tab) {
 
-  const checkCompleted = function (tabId, info) {
-    if (tab.id == tabId && info.status === 'complete') {
-      chrome.tabs.onUpdated.removeListener(checkCompleted);
-
-      chrome.pageCapture.saveAsMHTML({tabId: tab.id}, function(mhtmlBlob) {
-        var fr = new FileReader();
-        fr.onload = function () {
-            var mhtmlData = fr.result;
-            const html = mhtml2html.convert(mhtmlData);
-
-            const formData = new FormData();
-            formData.append("html", html.documentElement.outerHTML);
-            formData.append("title", tab.title)
-            $.ajax({
-                url: 'http://localhost:1111/save',
-                method: 'POST',
-                type: 'POST',
-                data: formData,
-                cache: false,
-                contentType: false,
-                processData: false,
-                complete: function() {
-                  alert("Page saved...")
-                }
-            });
-        };
-        fr.readAsBinaryString(mhtmlBlob);
-      });
-    }
-  }
-
-  chrome.tabs.onUpdated.addListener(checkCompleted);
+  // const checkCompleted = function (tabId, info) {
+  //   if (tab.id == tabId && info.status === 'complete') {
+  //     chrome.tabs.onUpdated.removeListener(checkCompleted);
+  //
+  //   }
+  // }
+  //
+  // chrome.tabs.onUpdated.addListener(checkCompleted);
 
   await chrome.tabs.executeScript({
     code: "\
@@ -43,5 +46,7 @@ chrome.browserAction.onClicked.addListener(async function(tab) {
       editor.scrollBy(0, scrollHeight);\
     "
   })
+
+  setTimeout(function() { savePage(tab) }, 500);
 
 });
